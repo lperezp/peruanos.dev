15776f7d5422a2f004ebf693c9d6e5a943bb6efa
Luis Eduardo <luiseperezp.10@gmail.com>
2025-01-30 16:23:13 -0500

    feat: update CardCommunityHome to display community data and enhance styles in Home page

--- app/components/card-community-home/card-community-home.tsx
+++ app/components/card-community-home/card-community-home.tsx
@@ -1,15 +1,16 @@
 import { ExternalLink } from 'lucide-react';
+import { ICommunity } from '@/app/models/community.model';
 
-export default function CardCommunityHome() {
+export default function CardCommunityHome({ community }: { community: ICommunity }) {
     return (
-        <div className="card-event-home p-6 bg-[var(--color-background)] border border-[var(--color-gray-light)] rounded-lg ">
-            <p className="text-[20px] color-[var(--color-foreground)] font-bold mb-1">LimaJS Meetup</p>
-            <p className="font-medium text-[var(--color-gray-light)]">Lima JS</p>
-            <p className="font-medium my-4 text-[var(--color-gray-light)]">La comunidad más grande de JavaScript en Lima. Meetups mensuales sobre React, Node.js, TypeScript y más.</p>
-            <div className="flex items-center gap-2 font-medium text-[var(--color-primary)] mb-1">
+        <div className="card-event-home p-6 bg-[var(--color-background)] border border-[var(--color-gray-light)] rounded-lg flex-1 max-w-sm">
+            <p className="text-[20px] color-[var(--color-foreground)] font-bold mb-1">{community.name}</p>
+            <p className="font-medium text-[var(--color-gray-light)]">{community.city}</p>
+            <p className="font-medium my-4 text-[var(--color-gray-light)] line-clamp-4">{community.description}</p>
+            <a href={community.contact.website} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-medium text-[var(--color-primary)] mb-1">
                 <span>Visitar sitio web</span>
                 <ExternalLink size={16} />
-            </div>
+            </a>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Link from 'next/link';
 import styles from './page.module.scss';
 import CardEventHome from './components/card-event-home/card-event-home';
 import CardCommunityHome from './components/card-community-home/card-community-home';
+import { LIST_COMMUNITIES } from './data/communities';
+import { ICommunity } from './models/community.model';
 
 export default function Home() {
   return (
     <main className="flex w-full max-w-7xl flex-col items-center bg-[var(--color-background)] mx-auto">
-      <section className="py-40 px-16 flex flex-col items-center " >
-        <h1 className="text-7xl text-center font-bold mb-9 leading-[1.4] w-[90%]">Conecta con <span className={`${styles.titleCustom} text-[var(--color-primary)]`}>la comunidad tech</span> en el Perú</h1>
-        <p className="text-center w-[70%] text-[20px]">Descubre eventos, únete a comunidades y contribuye a proyectos de código abierto realizados en Perú.</p>
-        <div className="flex flex-row gap-6 mt-12">
+      <section className={`${styles.hero} py-40 px-16 flex flex-col items-center`}>
+        <h1 className="text-7xl z-1 text-center font-bold mb-9 leading-[1.4] w-[90%]">Conecta con <span className={`${styles.titleCustom} text-[var(--color-primary)]`}>la comunidad tech</span> en el Perú</h1>
+        <p className="text-center z-1 w-[70%] text-[20px]">Descubre eventos, únete a comunidades y contribuye a proyectos de código abierto realizados en Perú.</p>
+        <div className="flex flex-row gap-6 mt-12 z-1">
           <Link className="px-6 py-3 bg-[var(--color-primary)] text-white rounded-full hover:bg-[var(--color-primary-hover)] transition" href='/events'>
             Ver próximos eventos
           </Link>
@@ -33,10 +35,13 @@ export default function Home() {
       <section className="py-15 w-full flex flex-col items-center">
         <h2 className="text-5xl text-center font-bold mb-9  w-[90%]">Comunidades</h2>
         <p className="text-center w-[70%] text-[20px]">Descubre y únete a las comunidades que impulsan la tecnología en el Perú.</p>
-        <div className="flex flex-row gap-6 m-10">
-          <CardCommunityHome />
-          <CardCommunityHome />
-          <CardCommunityHome />
+        <div className="flex flex-row gap-6 m-10 w-full justify-center">
+          {LIST_COMMUNITIES.slice(0, 3).map((community: ICommunity) => (
+            <CardCommunityHome
+              key={community.name}
+              community={community}
+            />
+          ))}
         </div>
         <Link className="px-6 py-3 bg-[var(--color-primary)] text-white rounded-full hover:bg-[var(--color-primary-hover)] transition" href='/communities'>
           Explorar comunidades
