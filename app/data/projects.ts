883c9ee0e3e1a8e31f253a029e5402735a40c083
Luis Eduardo <luiseperezp.10@gmail.com>
2026-01-02 18:49:14 -0500

    feat: update communities and events, removing outdated entries

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -4,13 +4,5 @@ export const PROJECTS: IProject[] = [
     {
         owner: 'lperezp',
         repo: 'peruanos.dev'
-    },
-    {
-        owner: 'pulumi',
-        repo: 'pulumi-go-provider'
-    },
-    {
-        owner: 'AngularPeru',
-        repo: 'demo-husky-prettier-eslint'
     }
 ];
